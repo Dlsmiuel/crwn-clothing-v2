80c29eff6c6416def13fdfa8ad5588f2d7308994
Dlsmiuel <38094761+Dlsmiuel@users.noreply.github.com>
2022-04-08 14:23:34 -0400

    migrated from contextAPI to redux, installed react-thunk to make asynchronous calls through middleware

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import { CartContext } from "../../contexts/cart.context";
+import { addItemToCart } from "../../store/cart/cart.action";
+import { selectCartItems } from "../../store/cart/cart.selector";
 
 import {
   ProductCardContainer,
@@ -11,9 +11,11 @@ import {
 } from "./product-card.styles.jsx";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
   const { name, price, imageUrl } = product;
-  const { addItemToCart } = useContext(CartContext);
-  const addProductToCart = () => addItemToCart(product);
+
+  const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
   return (
     <ProductCardContainer className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
