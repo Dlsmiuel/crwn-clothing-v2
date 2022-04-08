fb0e7bff1a78ec89a54560b253c59a20f45f2723
Dlsmiuel <38094761+Dlsmiuel@users.noreply.github.com>
2022-04-08 15:51:26 -0400

    added asynchronous behaviour to redux through redux-thunk to categories-redux

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,29 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selectors";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/category.selectors";
+
+import Spinner from "../../components/spinner/spinner.component";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
